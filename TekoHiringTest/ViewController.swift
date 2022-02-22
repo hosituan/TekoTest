88ab558217cc541381d2d58544cccfb6c460120f
HoSiTuan <tuan.ho@c2c-techhub.io>
2022-02-22 20:07:14 +0700

    Initial Commit

---
+++ TekoHiringTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TekoHiringTest
+//
+//  Created by Ho Si Tuan on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
